a2ede01a4ba365f0d8877e6a901d3f7b847430f0
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-09-16 11:23:29 +0300

    NFT dApp with the whitelist

---
+++ Sophomore/whitelist-dapp/hardhat/scripts/deploy-nft.js
@@ -0,0 +1,37 @@
+const hre = require("hardhat");
+
+const contractAddress = "0xFA8909E156840bA0aE81096276f7e0d9C43E439D";
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+  // Deploy the CryptoDevs Contract
+  const nftContract = await hre.ethers.deployContract("CryptoDevs", [
+    contractAddress,
+  ]);
+
+  // wait for the contract to deploy
+  await nftContract.waitForDeployment();
+
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", nftContract.target);
+
+  // Sleep for 30 seconds while Etherscan indexes the new contract deployment
+  await sleep(30 * 1000); // 30s = 30 * 1000 milliseconds
+
+  // Verify the contract on etherscan
+  await hre.run("verify:verify", {
+    address: nftContract.target,
+    constructorArguments: [contractAddress],
+  });
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
